edbcff07216ebada0fa9b6d15ac8928771a91085
Carlos Carranza <carlosscarranza@yahoo.com>
2020-03-31 21:04:28 -0500

    Last Changes

--- ChatClient/Program.cs
+++ ChatClient/Program.cs
@@ -17,7 +17,7 @@ namespace ChatClient
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormClient());
         }
     }
 }
--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading;
 
 namespace ChatServer
 {
@@ -58,7 +57,7 @@ namespace ChatServer
 
                 Console.WriteLine(dataFromClient + " Joined chat room ");
 
-                handleClinet client = new handleClinet();
+                HandleClient client = new HandleClient();
 
                 client.startClient(clientSocket, dataFromClient, clientsList);
 
@@ -118,93 +117,4 @@ namespace ChatServer
 
         }  //end broadcast function
     }//end Main class
-
-    public class handleClinet
-
-    {
-
-        TcpClient clientSocket;
-
-        string clNo;
-
-        Hashtable clientsList;
-
-
-
-        public void startClient(TcpClient inClientSocket, string clineNo, Hashtable cList)
-
-        {
-
-            this.clientSocket = inClientSocket;
-
-            this.clNo = clineNo;
-
-            this.clientsList = cList;
-
-            Thread ctThread = new Thread(doChat);
-
-            ctThread.Start();
-
-        }
-
-
-        private void doChat()
-
-        {
-
-            int requestCount = 0;
-
-            byte[] bytesFrom = new byte[10025];
-
-            string dataFromClient = null;
-
-            Byte[] sendBytes = null;
-
-            string serverResponse = null;
-
-            string rCount = null;
-
-            requestCount = 0;
-
-            while ((true))
-
-            {
-
-                try
-
-                {
-
-                    requestCount = requestCount + 1;
-
-                    NetworkStream networkStream = clientSocket.GetStream();
-
-                    networkStream.Read(bytesFrom, 0, (int)clientSocket.ReceiveBufferSize);
-
-                    dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
-
-                    dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
-
-                    Console.WriteLine("From client - " + clNo + " : " + dataFromClient);
-
-                    rCount = Convert.ToString(requestCount);
-
-
-
-                    Program.broadcast(dataFromClient, clNo, true);
-
-                }
-
-                catch (Exception ex)
-
-                {
-
-                    Console.WriteLine(ex.ToString());
-
-                }
-
-            }//end while
-
-        }//end doChat
-
-    } //end class handleClinet
 }
